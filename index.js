b1195d93312f45845bc3de8869ba7ccfa5e19fc3
Nicolai Gorden <nicolaigorden@gmail.com>
2021-10-18 19:56:38 -0400

    commit tree 768a5840da4dca348195e1f17fd93dc397d95713

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+const Hq = 42
+const Feet = 264
+let distanceTravelled
+
+function distanceFromHqInBlocks(customerStreet){
+    let distanceInBlocks = Math.abs(customerStreet-Hq)
+    return distanceInBlocks
+}
+
+function distanceFromHqInFeet(customerStreet){
+    return distanceFromHqInBlocks(customerStreet)*Feet;
+}
+
+function distanceTravelledInFeet(start, destination){
+    distanceTravelled = Math.abs(start-destination)*Feet;
+    return distanceTravelled
+}
+
+function calculatesFarePrice(start, destination){
+    distanceTravelledInFeet(start, destination)
+    if (distanceTravelled < 400){
+        return 0
+    }else if (distanceTravelled >= 401 && distanceTravelled <= 2000) {
+        return (distanceTravelled-400)*.02
+    }else if (distanceTravelled >= 2001 && distanceTravelled <= 2500) {
+        return 25
+    }else {
+        return `cannot travel that far`
+    }
+}
